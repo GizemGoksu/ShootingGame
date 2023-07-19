8596e5dc56f643d645e3cb5356dc1f5f752e583f
GizemGoksu <gizemgks_282@hotmail.com>
2023-07-19 15:29:45 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'screen/main_page.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
   }
 }
